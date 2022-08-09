0b99ff7e8bf6c37d77522dde100e887d870986f0
Pedro Mendoza <pedro.mendoza.g@gmail.com>
2022-08-08 21:41:31 -0600

    List and Insert

---
+++ EstudenCRUD/Controllers/StudentController.cs
@@ -0,0 +1,62 @@
+﻿using EstudenCRUD.Context;
+using EstudenCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace EstudenCRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        EstudentContext db = new EstudentContext();
+
+        [HttpGet]
+        public Response GetStudent()
+        {
+            try
+            {
+                var data = (from student in db.Estudents
+                            select student).ToArray();
+
+                Array.Reverse(data);
+
+                return new Response { Data = data, Success = true };
+            }
+            catch (System.Exception e)
+            {
+                return new Response { Message = "Error, sonsulte con el administrador", Success = false };
+            }
+        }
+
+
+        [HttpPost]
+        public Response InserStudent([FromBody]Estudents student)
+        {
+
+            if (student.Apellido.Trim().Equals("") || student.Nombre.Trim().Equals(""))
+            {
+                return new Response { Success = false, Message = "No se suministraron todos los datos necesarios" };
+            }
+
+            try
+            {
+                db.Estudents.Add(student);
+
+                db.SaveChanges();
+
+                return new Response { Data=student, Message="Estudiante Registrado!", Success = true };
+            }
+            catch (System.Exception)
+            {
+
+                return new Response { Success = false, Message = "Error al ejecutar la tarea, consulte al administrador" };
+            }
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EstudenCRUD/wwwroot/js/student.js
@@ -0,0 +1,75 @@
+﻿async function editStudent(idStudent) {
+
+    alert(idStudent);
+}
+
+function addRow(student) {
+    var tBodyRef = document.getElementById('student').getElementsByTagName('tbody')[0];
+
+    var row = tBodyRef.insertRow(0);
+
+    var idCell = row.insertCell(0);
+    var nombreCell = row.insertCell(1);
+    var apellidoCell = row.insertCell(2);
+    var actionCell = row.insertCell(3);
+
+    idCell.innerHTML = student.idStudent;
+    nombreCell.innerHTML = student.nombre;
+    apellidoCell.innerHTML = student.apellido;
+    actionCell.innerHTML = `<button class="btn btn-primary mx-1" onclick="editStudent(${student.idStudent})">Editar</button><button class="btn btn-danger mx-1" onclick="delStudent(${student.idStudent})")>Eliminar</button>` 
+
+}
+
+async function loadStudent() {
+
+    fetch('https://localhost:44338/Student/GetStudent')
+        .then(response => response.json())
+        .then(data => {            
+              data.data.forEach(addRow)
+        })
+}
+
+
+async function inserStudent() {
+
+    var nombre = document.getElementById('NombreAdd').value;
+    var apellido = document.getElementById('ApellidoAdd').value;
+
+    if (nombre.trim() === '' || apellido.trim() === '') {
+        alert('Nombre y Apellido son datos requeridos');
+        return;
+    }
+
+    var student = {
+        Nombre: nombre,
+        Apellido: apellido
+    }
+
+    fetch('https://localhost:44338/Student/InserStudent', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(student)
+    }).then(response => response.json())
+        .then(
+            data => {
+                alert(data.message)
+                var tBodyRef = document.getElementById('student').getElementsByTagName('tbody')[0].innerHTML='';
+
+                document.getElementById('NombreAdd').value = ''; 
+                document.getElementById('ApellidoAdd').value = '';
+
+                loadStudent();
+
+                $('#modalAdd').modal('hide');
+            }
+    )
+
+    
+    return;
+    
+}
+
+
+loadStudent();
